4357891e30e00f454c383c4b1a038150d8a9353e
parkeerwacht <jurre@coolmail.nl>
2017-03-27 18:30:55 +0200

    Create mainstage.js

---
+++ mainstage.js
@@ -0,0 +1,120 @@
+(function () {
+    API.sendChat("Loading Meep-Bot V2.0");
+
+    // Change this to your GitHub username so you don't have to modify so many things.
+    var fork = "parkeerwacht";
+
+    // Define our function responsible for extending the bot.
+    function extend() {
+        // If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+          return setTimeout(extend, 1 * 1000);
+        }
+
+        // Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        // Load custom settings set below
+        bot.retrieveSettings();
+
+        //Extend the bot here, either by calling another function or here directly.
+
+        // You can add more spam words to the bot.
+        var spamWords = ['spam1', 'spam2', 'spam3', 'spam4'];
+        for (var i = 0; i < spamWords.length; i++) {
+          window.bot.chatUtilities.spam.push(spamWords[i]);
+        }
+
+        // Example code for a bot command:
+        bot.commands.baconCommand = {
+          command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("/me Bacon!!!");
+            }
+          }
+        };
+
+        // Load the chat package again to account for any changes
+        bot.loadChat();
+
+      }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+      botName: "AGM-Bot",
+      language: "english",
+      chatLink: "https://rawgit.com/parkeerwacht/custom/master/lang/mainstage.json",
+      scriptLink: "https://rawgit.com/parkeerwacht/custom/master/basicBot-mainstage.js",
+      roomLock: false, // Requires an extension to re-load the script
+      startupCap: 1, // 1-200
+      startupVolume: 0, // 0-100
+      startupEmoji: false, // true or false
+      autowoot: true,
+      autoskip: false,
+      smartSkip: true,
+      cmdDeletion: false,
+      maximumAfk: 60,
+      afkRemoval: true,
+      maximumDc: 30,
+      bouncerPlus: true,
+      blacklistEnabled: false,
+      lockdownEnabled: false,
+      lockGuard: false,
+      maximumLocktime: 10,
+      cycleGuard: true,
+      maximumCycletime: 10,
+      voteSkip: false,
+      voteSkipLimit: 10,
+      historySkip: true,
+      timeGuard: true,
+      maximumSongLength: 10,
+      autodisable: false,
+      commandCooldown: 30,
+      usercommandsEnabled: true,
+      skipPosition: 1,
+      skipReasons: [
+      ["theme", "This song does not fit the room theme. "],
+      ["op", "This song is on the OP list. "],
+      ["history", "This song is in the history. "],
+      ["10min", "The song you played is too long (10 mimnute max) "],
+      ["sound", "The song you played had bad sound quality or no sound. "],
+      ["nsfw", "The song you contained was NSFW (image or sound). "],
+      ["dead", "The song you played was not available for some users. "],
+      ["rip", "The song you played was not available for some users. "],
+      ["unavailable", "The song you played was not available for some users. "]
+      ],
+      afkpositionCheck: 15,
+      afkRankCheck: "ambassador",
+      motdEnabled: false,
+      motdInterval: 5,
+      motd: "Temporary Message of the Day",
+      filterChat: false,
+      etaRestriction: false,
+      welcome: true,
+      opLink: null,
+      rulesLink: "https://akari.in/AGM-Rules",
+      themeLink: "https://akari.in/AGM-Rules",
+      fbLink: null,
+      youtubeLink: null,
+      website: null,
+      intervalMessages: [],
+      messageInterval: 5,
+      songstats: true,
+      commandLiteral: "!",
+      blacklists: {
+        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+      }
+    }));
+
+    // Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/parkeerwacht/custom/master/basicBot-mainstage.js", extend);
+
+}).call(this);
